4663686b3c3e8378317a4f5a78c4b7b122544be8
ryujiin <ryujiin22@gmail.com>
2014-07-19 16:29:51 -0500

    agregando nuevo region

--- Loviz/templates/page.tpl.php
+++ Loviz/templates/page.tpl.php
@@ -11,22 +11,25 @@
 <div id="page">
 
   <header class="header" id="header" role="banner">
-    <?php if ($secondary_menu): ?>
-      <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
-          'attributes' => array(
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => $secondary_menu_heading,
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
-      </nav>
-    <?php endif; ?>
-
+    <div class="header-top">
+      <?php print render($page['header-top']); ?>
+      <?php if ($secondary_menu): ?>
+        <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
+          <?php print theme('links__system_secondary_menu', array(
+            'links' => $secondary_menu,
+            'attributes' => array(
+              'class' => array('links', 'inline', 'clearfix'),
+            ),
+            'heading' => array(
+              'text' => $secondary_menu_heading,
+              'level' => 'h2',
+              'class' => array('element-invisible'),
+            ),
+          )); ?>
+        </nav>
+      <?php endif; ?>  
+    </div>
+    
     <div class="container-fluid">
       <div class="row">
         <div class="col-sm-4">
