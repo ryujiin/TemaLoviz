024bad7bae938b1b7b5efda4fa5ccdce87e5a949
ryujiin <ryujiin22@gmail.com>
2014-07-26 13:56:50 -0500

    arreglar page.tpl2

--- Loviz/templates/page.tpl.php
+++ Loviz/templates/page.tpl.php
@@ -32,7 +32,7 @@
             <div class="clear"></div>
           </div>
           
-          <div class="container-fluid header-main">
+          <div class="header-main">
             <div class="row">
               <div class="col-sm-4">
 
@@ -62,8 +62,37 @@
           </div>
         </header>
 
-        <div id="main">
+        <div id="navigation">
+            <div class="row">
+              <div class="col-sm-8">
+                <?php if ($main_menu): ?>
+                  <nav id="main-menu" role="navigation" tabindex="-1">
+                    <?php
+                    // This code snippet is hard to modify. We recommend turning off the
+                    // "Main menu" on your sub-theme's settings form, deleting this PHP
+                    // code block, and, instead, using the "Menu block" module.
+                    // @see https://drupal.org/project/menu_block
+                    print theme('links__system_main_menu', array(
+                      'links' => $main_menu,
+                      'attributes' => array(
+                        'class' => array('links', 'inline', 'clearfix'),
+                      ),
+                      'heading' => array(
+                        'text' => t('Main menu'),
+                        'level' => 'h2',
+                        'class' => array('element-invisible'),
+                      ),
+                    )); ?>
+                  </nav>
+                <?php endif; ?>
+              </div>
+              <div class="col-sm-4">
+                <?php print render($page['navigation']); ?>                
+              </div>
+            </div>
+        </div>
 
+        <div id="main">
           <div id="content" class="column" role="main">
             <?php print render($page['highlighted']); ?>
             <?php print $breadcrumb; ?>
@@ -82,34 +111,6 @@
             <?php print render($page['content']); ?>
             <?php print $feed_icons; ?>
           </div>
-
-          <div id="navigation">
-
-            <?php if ($main_menu): ?>
-              <nav id="main-menu" role="navigation" tabindex="-1">
-                <?php
-                // This code snippet is hard to modify. We recommend turning off the
-                // "Main menu" on your sub-theme's settings form, deleting this PHP
-                // code block, and, instead, using the "Menu block" module.
-                // @see https://drupal.org/project/menu_block
-                print theme('links__system_main_menu', array(
-                  'links' => $main_menu,
-                  'attributes' => array(
-                    'class' => array('links', 'inline', 'clearfix'),
-                  ),
-                  'heading' => array(
-                    'text' => t('Main menu'),
-                    'level' => 'h2',
-                    'class' => array('element-invisible'),
-                  ),
-                )); ?>
-              </nav>
-            <?php endif; ?>
-
-            <?php print render($page['navigation']); ?>
-
-          </div>
-
           <?php
             // Render the sidebars to see if there's anything in them.
             $sidebar_first  = render($page['sidebar_first']);
@@ -124,15 +125,12 @@
           <?php endif; ?>
 
         </div>
-
-        <?php print render($page['footer']); ?>
-
       </div>
 
       <?php print render($page['bottom']); ?>
     </div>
     <footer id="footer">
-      
+      <?php print render($page['footer']); ?>
     </footer>
   </div>
 </div>
